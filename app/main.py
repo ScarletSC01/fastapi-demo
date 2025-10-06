00c83f9dc8054ac21498d2d47b903e36fcea5a69
ScarletSC01 <142684394+ScarletSC01@users.noreply.github.com>
2025-10-06 13:05:12 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,4 +4,10 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello World!"}
+    return {"message": "Hello from Cloud Run!"}
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
